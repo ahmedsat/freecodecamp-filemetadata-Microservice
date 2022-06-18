10f015f8f72457959a62b63d4970d74177e46b94
ahmedsat <ahmedsat303@gmail.com>
2022-06-18 12:51:18 +0000

    done

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # File Metadata Microservice
 
-This is the boilerplate for the File Metadata Microservice project. Instructions for building your project can be found at https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice
+## freecodecamp.org
+
+This Microservice is a simple file metadata microservice.
+
+it's task required for the [File Metadata Microservice](https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/file-metadata-microservice).
--- index.js
+++ index.js
@@ -1,20 +1,42 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+const fileUpload = require("express-fileupload");
+const path = require("path");
+var cors = require("cors");
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(fileUpload());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", function (req, res) {
+  if (Object.keys(req.files).length == 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
 
+  const uploadedFile = req.files.upfile;
+  const name = uploadedFile.name;
+  const type = uploadedFile.mimetype;
+  const size = uploadedFile.size;
 
+  const response = {
+    name,
+    type,
+    size,
+  };
+
+  console.log(response);
+  res.json(response);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
